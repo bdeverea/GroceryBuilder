f25caa51f0c5e1b25f242ebeb5bde1ae56a9e721
Bryan D <bdeverea@gmail.com>
2010-05-20 12:31:58 -0400

    updating regex to handle item descriptions in parens

--- lib/ingredient_parser.rb
+++ lib/ingredient_parser.rb
@@ -1,10 +1,22 @@
 class IngredientParser
   def initialize
-    @pat = /(\d+)(\s+|\/\d+\s+)(ounce|oz|cup|pound|teaspoon|tablespoon|slice|clove)*/
+    @pat = /(^\d+)(\/\d+)? (\(.+\) )?(ounce|oz|cup|pound|lb|teaspoon|tsp|tablespoon|tblsp|liter|gallon|quart|pint|slice|clove)?(s? |\s*)(.+)/
+    # @pat = /(^\d+)(\/?\d+)?(\(.*\))(ounce|oz|cup|pound|lb|teaspoon|tsp|tablespoon|tblsp|liter|gallon|quart|pint|slice|clove)(s?|\s*)(.+)/
   end
   
   def parse(str)
-    @pat.match(str)
+    ingredients = []
+    match = @pat.match(str)
+    str = str
+    
+    str.lines.each do |l|
+        l.match(@pat)
+        # ingredients["#{$5}".strip] = {:quantity => "#{$1}#{$2}".strip, :unit => "#{$3}".strip} #use item name as key and add quantity and units to ingredients hash
+        #TODO: Strip whitespace from result set without having to apply .stip to each string
+        ingredients << ["#{$1}#{$2}", "#{$4}", "#{$3}#{$6}"]
+    end
+    p ingredients
+    return ingredients
   end
   
 end
@@ -44,10 +56,5 @@ str = "1 (8 ounce) package angel hair pasta
 1 cup white sugar"
 
 parser = IngredientParser.new
-puts parser.parse(str)
-
-str.lines do |s| 
-  s.each do 
-    puts parser.parse(s)
-  end
-end
+result = parser.parse(str)
+# p result
