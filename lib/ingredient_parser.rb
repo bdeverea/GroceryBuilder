157e4c86b9cd0b66cd37ab6ce5d5c7a2f9eaab79
Bryan D <bdeverea@gmail.com>
2010-05-26 07:50:45 -0400

    updating regex and comments to handle floating point integers in quantity field

--- lib/ingredient_parser.rb
+++ lib/ingredient_parser.rb
@@ -1,60 +1,72 @@
 class IngredientParser
   def initialize
-    @pat = /(^\d+)(\/\d+)? (\(.+\) )?(ounce|oz|cup|pound|lb|teaspoon|tsp|tablespoon|tblsp|liter|gallon|quart|pint|slice|clove)?(s? |\s*)(.+)/
-    # @pat = /(^\d+)(\/?\d+)?(\(.*\))(ounce|oz|cup|pound|lb|teaspoon|tsp|tablespoon|tblsp|liter|gallon|quart|pint|slice|clove)(s?|\s*)(.+)/
+    # INGREDIENT PARSER REGEX
+    # $1 one or more digits at the start of the line
+    # $2 one or zero occurences of one or zero /'s OR .'s OR space 
+    # $3 one or zero occurences of one or more digits
+      # $1 + $2 + $3 = total quantity of ingredient (i.e. "4" or "1/2" or "1.5")
+    # space
+    # $4 one or zero occurences of any characters surrounded by parenthesis = item description (i.e. "(8 ounce package)") 
+    # $5 one or zero occurences of a specific unit of measure = unit
+    # $6 one or zero "s" to remove plurarility from unit OR one or more spaces (currently not used in return value)
+    # $7 one or more characters = item
+    @pat = /(^\d+)(\.|\/|\s)?(\d+)? (\(.+\) )?(ounce|oz|cup|pound|lb|teaspoon|tsp|tablespoon|tblsp|liter|gallon|quart|pint|slice|clove)?(s? |\s*)(.+)/
   end
   
   def parse(str)
     ingredients = []
+    
     match = @pat.match(str)
     str = str
-    
+  
     str.lines.each do |l|
-        l.match(@pat)
-        # ingredients["#{$5}".strip] = {:quantity => "#{$1}#{$2}".strip, :unit => "#{$3}".strip} #use item name as key and add quantity and units to ingredients hash
-        #TODO: Strip whitespace from result set without having to apply .stip to each string
-        ingredients << ["#{$1}#{$2}", "#{$4}", "#{$3}#{$6}"]
+        l.strip.match(@pat)
+        ingredients << ["#{$1}#{$2}#{$3}", "#{$5}", "#{$4}#{$7}"]
     end
+
     p ingredients
     return ingredients
+
   end
   
 end
 
+# str = "1 (8 ounce) package angel hair pasta
+# 2.5 cups baking soda
+# 1/2 cup butter
+# 4 cloves minced garlic
+# 1 pound shrimp, peeled and deveined
+# 1 cup dry white wine
+# 1/4 teaspoon ground black pepper
+# 3/4 cup grated Parmesan cheese
+# 1 tablespoon chopped fresh parsley
+# 6 cups romaine lettuce
+# 1/2 small red onion, diced
+# 3/4 cup grape tomatoes, cut in half
+# 3 slices Wonder® Classic White Bread
+# 1/2 cup light mayonnaise
+# 1 tablespoon lemon juice
+# 1 clove garlic, minced
+# 1/4 teaspoon seasoned salt
+# 6 slices bacon, cooked crisp and crumbled
+# 3 cups all-purpose flour
+# 2 cups white sugar
+# 3 teaspoons baking soda
+# 6 tablespoons unsweetened cocoa powder
+# 1 teaspoon salt
+# 2/3 cup vegetable oil
+# 3 tablespoons distilled white vinegar
+# 2 teaspoons vanilla extract
+# 2 cups water
+# 1/2 cup butter, softened
+# 1/2 cup shortening
+# 2/3 cup milk
+# 1 teaspoon vanilla extract
+# 1 cup white sugar"
+# 
 
-
-str = "1 (8 ounce) package angel hair pasta
-1/2 cup butter
-4 cloves minced garlic
-1 pound shrimp, peeled and deveined
-1 cup dry white wine
-1/4 teaspoon ground black pepper
-3/4 cup grated Parmesan cheese
-1 tablespoon chopped fresh parsley
-6 cups romaine lettuce
-1/2 small red onion, diced
-3/4 cup grape tomatoes, cut in half
-3 slices Wonder® Classic White Bread
-1/2 cup light mayonnaise
-1 tablespoon lemon juice
-1 clove garlic, minced
-1/4 teaspoon seasoned salt
-6 slices bacon, cooked crisp and crumbled
-3 cups all-purpose flour
-2 cups white sugar
-3 teaspoons baking soda
-6 tablespoons unsweetened cocoa powder
-1 teaspoon salt
-2/3 cup vegetable oil
-3 tablespoons distilled white vinegar
-2 teaspoons vanilla extract
-2 cups water
-1/2 cup butter, softened
-1/2 cup shortening
-2/3 cup milk
-1 teaspoon vanilla extract
-1 cup white sugar"
-
-parser = IngredientParser.new
-result = parser.parse(str)
+# num = 1234567890
+# 
+# parser = IngredientParser.new
+# result = parser.parse(num)
 # p result
